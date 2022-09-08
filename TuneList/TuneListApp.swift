438c1d6168e2dc6882cf250d7a667a25339f1391
Marc Schwartz <marcschwartz1990@gmail.com>
2022-09-08 15:25:46 -0400

    Initial Commit

---
+++ TuneList/TuneListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneListApp.swift
+//  TuneList
+//
+//  Created by Marc-Developer on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TuneListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
